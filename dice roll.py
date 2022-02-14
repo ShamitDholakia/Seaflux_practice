a8384c8d906bdf5d206babedab22cf0b046fe17e
ShamitDholakia <99485729+ShamitDholakia@users.noreply.github.com>
2022-02-14 13:22:22 +0530

    Update dice roll.py

--- dice roll.py
+++ dice roll.py
@@ -1,14 +1,19 @@
-def roll(dice):
-    while dice==1:
-        b=int(input("press 1 or 0"))
-        if b==1:
-            print("roll")
-        if b==0:
-            return "exit"   
-a=int(input("press 1 to roll and 0 to exit"))
-roll(a)
-    
-
-
-
-
+import random 
+x = "1"
+while x == "1":
+    no = random.randint(1,6)
+    if no == 1:
+        print("*")
+    if no == 2:
+        print("* * ")
+    if no == 3:
+        print("* * *")
+    if no == 4:
+        print("* * * *")
+    if no == 5:
+        print("* * * * *")
+    if no == 6:
+       print("* * * * * *")
+         
+    x=input("press 1 to roll again and 0 to exit:")
+    print("\n")
